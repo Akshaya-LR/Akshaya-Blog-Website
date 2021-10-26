610f84a4a42d99fd0f82a62978c1d7973516a0fb
Akshaya <akshayasharma014@gmail.com>
2021-10-26 13:42:07 +0530

    added comments in main.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
+# Import the required Packages
 from flask import Flask, render_template, request
 import requests
 import smtplib
 import os
 
-
+# Initialize the Flask application
 app = Flask(__name__)
 
-
+# Variables used
 message = "Successfully sent your message!!"
 sub_msg = "Have anymore questions?"
-contact_head = "Message Me!"
+contact_head = "Contact Me!"
 contact_sub = "Send me a quick message!"
 
-get_post = os.environ.get("API")
+# API for the blog posts
+get_post = "https://api.npoint.io/b9110dd9f30a652d5da0"
 blog_p = requests.get(get_post)
 posts = blog_p.json()
 
-
+# Use environment variables for personal info
 email = os.environ.get("EMAIL")
 pwd = os.environ.get("PWD")
 
 
+# Home page route
 @app.route('/')
 def home():
     return render_template('index.html', blogpost=posts)
 
 
+# Blog post route
 @app.route('/post/<int:num>')
 def post(num):
     post_requested = None
@@ -40,11 +44,13 @@ def post(num):
                            posts=post_requested)
 
 
+# About page route
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
+# Send Email function from Contact page using SMTP
 def email_me(name, mail, phone, msg):
     email_message = f"Subject:New Message from Blog Website - User\n\nName: {name}\nEmail: {mail}\nPhone: {phone}\nMessage:{msg}"
     with smtplib.SMTP("smtp.gmail.com", 587) as connection:
@@ -54,15 +60,17 @@ def email_me(name, mail, phone, msg):
         connection.sendmail(email, email, email_message)
 
 
+# Contact page route
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
         user_data = request.form
-        print(user_data["name"])
+        # print(user_data["name"])
         email_me(user_data["name"], user_data["email"], user_data["phone_number"], user_data["message"])
         return render_template('contact.html', heading=message, sub_heading=sub_msg)
     return render_template('contact.html', heading=contact_head, sub_heading=contact_sub)
 
 
+# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
